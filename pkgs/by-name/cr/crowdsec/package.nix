508ac4fe22485d7813c3ac3ee6cf4c93451dd245
TornaxO7 <tornax@proton.me>
2026-01-21 14:21:46 +0100

    nixos/crowdsec: refactor
    Co-authored-by: Nicolas Mémeint <git@nicomem.com>
    Co-authored-by: Jarno Pelkonen <23549925+jarppiko@users.noreply.github.com>
    Co-authored-by: Yash Garg <me@yashgarg.dev>
    Co-authored-by: Gaël James <gaeljames@gmail.com>
    Co-authored-by: Azat Bahawi <azat@bahawi.net>
    (cherry picked from commit 548fd1efa5417a716dfce649f1c379379b615ec9)

--- pkgs/by-name/cr/crowdsec/package.nix
+++ pkgs/by-name/cr/crowdsec/package.nix
@@ -23,6 +23,13 @@ buildGoModule rec {
   subPackages = [
     "cmd/crowdsec"
     "cmd/crowdsec-cli"
+    "cmd/notification-dummy"
+    "cmd/notification-email"
+    "cmd/notification-file"
+    "cmd/notification-http"
+    "cmd/notification-sentinel"
+    "cmd/notification-slack"
+    "cmd/notification-splunk"
   ];
 
   ldflags = [
@@ -42,9 +49,14 @@ buildGoModule rec {
     mkdir -p $out/share/crowdsec
     cp -r ./config $out/share/crowdsec/
 
-    mkdir -p $out/lib/systemd/system
-    substitute ./config/crowdsec.service $out/lib/systemd/system/crowdsec.service \
-      --replace-fail /usr/local $out
+    mkdir -p $out/libexec/crowdsec/plugins
+    mv $out/bin/notification-dummy $out/libexec/crowdsec/plugins/
+    mv $out/bin/notification-email $out/libexec/crowdsec/plugins/
+    mv $out/bin/notification-file $out/libexec/crowdsec/plugins/
+    mv $out/bin/notification-http $out/libexec/crowdsec/plugins/
+    mv $out/bin/notification-sentinel $out/libexec/crowdsec/plugins/
+    mv $out/bin/notification-slack $out/libexec/crowdsec/plugins/
+    mv $out/bin/notification-splunk $out/libexec/crowdsec/plugins/
 
     installShellCompletion --cmd cscli \
       --bash <($out/bin/cscli completion bash) \
