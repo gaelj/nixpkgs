d36a9aff6fde31ae0baae8b197dba4780305139a
Gaël James <gaeljames@gmail.com>
2026-03-19 22:35:38 +0100

    gotify-server: systemd hardening

--- nixos/modules/services/web-apps/gotify-server.nix
+++ nixos/modules/services/web-apps/gotify-server.nix
@@ -82,6 +82,40 @@ in
         Restart = "always";
         DynamicUser = true;
         ExecStart = lib.getExe cfg.package;
+
+        ProtectSystem = "strict";
+        ProtectHome = true;
+        PrivateTmp = true;
+        PrivateDevices = true;
+        ProtectKernelTunables = true;
+        ProtectKernelModules = true;
+        ProtectKernelLogs = true;
+        ProtectControlGroups = true;
+        ProtectProc = "invisible";
+        ProcSubset = "pid";
+
+        NoNewPrivileges = true;
+        CapabilityBoundingSet = "";
+        AmbientCapabilities = "";
+        RestrictSUIDSGID = true;
+        RestrictNamespaces = true;
+        SystemCallFilter = [ "@system-service" ];
+        SystemCallErrorNumber = "EPERM";
+        SystemCallArchitectures = "native";
+
+        RestrictAddressFamilies = [
+          "AF_INET"
+          "AF_INET6"
+          "AF_UNIX"
+        ];
+
+        MemoryDenyWriteExecute = true;
+        RestrictRealtime = true;
+        LockPersonality = true;
+        RemoveIPC = true;
+
+        LimitNOFILE = 4096;
+        LimitNPROC = 64;
       };
     };
   };
