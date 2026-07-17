34a76a1dc8111c23bcfa7175d5200e2021b39ce5
Gaël James <gaeljames@gmail.com>
2026-07-17 19:27:53 +0200

    python3Packages.sqlacodegen: init at 4.0.4

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -19741,6 +19741,8 @@ self: super: with self; {
 
   sqids = callPackage ../development/python-modules/sqids { };
 
+  sqlacodegen = callPackage ../development/python-modules/sqlacodegen { };
+
   sqlalchemy = callPackage ../development/python-modules/sqlalchemy { };
 
   sqlalchemy-adapter = callPackage ../development/python-modules/sqlalchemy-adapter { };
